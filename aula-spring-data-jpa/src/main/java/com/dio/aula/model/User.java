ab1a804348d806d01f2c33f9ce626cb1056841a1
arduinobymyself <arduinobymyself@gmail.com>
2022-10-18 15:44:29 -0300

    subindo arquivos curso dio springboot

---
+++ aula-spring-data-jpa/src/main/java/com/dio/aula/model/User.java
@@ -0,0 +1,51 @@
+package com.dio.aula.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String passsword;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasssword() {
+        return passsword;
+    }
+
+    public void setPasssword(String passsword) {
+        this.passsword = passsword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", passsword='" + passsword + '\'' +
+                '}';
+    }
+}
---
+++ primeiros-passos/src/main/java/dio/primeirospassos/springboot/Calculadora.java
@@ -0,0 +1,2 @@
+package dio.primeirospassos.springboot;public class Calculadora {
+}
